d1eedacb1c82f42426dcb6b7147d849c1ae3313e
pChitral <chitral.patil@vit.edu.in>
2023-05-02 13:26:22 -0400

    Added submit button

--- frontend/src/MovieRecommendations.js
+++ frontend/src/MovieRecommendations.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { getMovieRecommendations } from "./api/api";
 
 function MovieRecommendations() {
+  const [movieName, setMovieName] = useState("");
   const [recommendedMovies, setRecommendedMovies] = useState([]);
 
-  useEffect(() => {
-    async function fetchRecommendedMovies() {
-      const data = await getMovieRecommendations("Avatar");
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    if (movieName) {
+      const data = await getMovieRecommendations(movieName);
       const recommendedMovies = data.recommended_movie_names.map(
         (name, index) => {
           return { name, posterUrl: data.recommended_movie_posters[index] };
@@ -14,21 +16,34 @@ function MovieRecommendations() {
       );
       setRecommendedMovies(recommendedMovies);
     }
+  };
 
-    fetchRecommendedMovies();
-  }, []);
+  const handleInputChange = (event) => {
+    setMovieName(event.target.value);
+  };
 
   return (
     <div>
       <h1>Recommended Movies</h1>
-      <ul>
-        {recommendedMovies.map((movie, index) => (
-          <li key={index}>
-            <img src={movie.posterUrl} alt={movie.name} />
-            <p>{movie.name}</p>
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Movie name:
+          <input type="text" value={movieName} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {recommendedMovies.length > 0 ? (
+        <ul>
+          {recommendedMovies.map((movie, index) => (
+            <li key={index}>
+              <img src={movie.posterUrl} alt={movie.name} />
+              <p>{movie.name}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No recommended movies to display.</p>
+      )}
     </div>
   );
 }
